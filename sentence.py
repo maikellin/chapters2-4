44dde7ae6ab08374b299aa580da1e94f50b9ffbd
David Aronson <davidaronson13@gmail.com>
2017-09-14 12:41:18 -0400

    getting started
    just startting

---
+++ sentence.py
@@ -0,0 +1,7 @@
+# this is th esentance I'm worming with:
+#All work and no play makes Jack a dull boy
+#I'm going to mak ethese variables
+word1 = "All"
+word2 = "work"
+print(word1 + " " + word2) 
+
